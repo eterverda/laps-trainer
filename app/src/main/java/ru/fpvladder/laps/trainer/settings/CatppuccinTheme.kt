071a3d7c8ff86c1823fc2709f94ebd3527a2e998
Alexander Asanov <aleksandro.eterverda@gmail.com>
2026-06-16 15:26:39 +0300

    Removed unused enum

--- app/src/main/java/ru/fpvladder/laps/trainer/settings/CatppuccinTheme.kt
+++
@@ -1,8 +0,0 @@
-package ru.fpvladder.laps.trainer.settings
-
-enum class CatppuccinTheme(val displayName: String) {
-    LATTE("Latte"),
-    FRAPPE("Frappe"),
-    MACCHIATO("Macchiato"),
-    MOCHA("Mocha")
-}
