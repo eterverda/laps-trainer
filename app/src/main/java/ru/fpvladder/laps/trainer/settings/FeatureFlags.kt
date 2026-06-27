fe6e8e332685ea086f190815afdace413fac2b7d
Alexander Asanov <aleksandro.eterverda@gmail.com>
2026-06-27 10:40:04 +0300

    Enabled usb-keyboard

--- app/src/main/java/ru/fpvladder/laps/trainer/settings/FeatureFlags.kt
+++ app/src/main/java/ru/fpvladder/laps/trainer/settings/FeatureFlags.kt
@@ -7,4 +7,4 @@ package ru.fpvladder.laps.trainer.settings
  * in the source and can be re-enabled by flipping the flag.
  */
 val TEAM_HOLESHOT_ENABLED = "false".toBoolean()
-val USB_ENABLED = "false".toBoolean()
+val USB_ENABLED = "true".toBoolean()
